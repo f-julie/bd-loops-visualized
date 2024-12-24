c571372aa107892d870ba5a8ca9c46494d52de96
Julie Fiess <juliefiess@gmail.com>
2024-12-24 09:55:44 -0600

    Completed

--- src/processing/sketches/Main.java
+++ src/processing/sketches/Main.java
@@ -44,20 +44,117 @@ public class Main extends PApplet {
         menu();
     }
 
+    /**
+     * Draw horizontally lined paper
+     * @param leftMargin the number of pixels to the left of the vertical line
+     * @param topMargin the number of pixels above the first horizontal line
+     * @param gapHeight the number of pixels between each horizontal line
+     */
     public void drawLinedPaper(int leftMargin, int topMargin, int gapHeight) {
-        // TODO: Finish this method.
+        stroke(0, 30, 100);
+        line(leftMargin, 0, leftMargin, height);
+
+        stroke(240, 30, 100);
+
+        //While loop solution
+        /*
+        int lineNumber = 0;
+        while (topMargin + lineNumber * gapHeight < height) {
+            line(0, topMargin + lineNumber * gapHeight, width, topMargin + lineNumber * gapHeight);
+            lineNumber++;
+        }
+        */
+
+        //Another while loop solution
+        //Note: In while loop, yCoord is still available outside the loop
+        /*
+        int yCoord = topMargin;
+        while (yCoord < height) {
+            line(0, yCoord, width, yCoord);
+            yCoord += gapHeight;
+        }
+        */
+
+        //Note: In for loop, yCoord is local (within curly brackets)
+        for ( int yCoord = topMargin; yCoord < height; yCoord += gapHeight) {
+            line(0, yCoord, width, yCoord);
+        }
     }
 
+    /**
+     * Draws graph paper
+     * @param boxLength the number of pixels between horizontal and vertical lines
+     * @param isEngineer true if every 5th line should be bold, false otherwise
+     */
     public void drawGridPaper(int boxLength, boolean isEngineer) {
-        // TODO: Finish this method.
+        // Vertical
+        stroke(0, 30, 100);
+
+        int lineNumber = 0;
+        for ( int x = boxLength; x < width; x += boxLength) {
+            if (lineNumber % 5 == 0) {
+                strokeWeight(3);
+            } else {
+                strokeWeight(1);
+            }
+            line(x, 0, x, height);
+            lineNumber++;
+        }
+
+        // Horizontal
+        stroke(230, 30, 100);
+
+        lineNumber = 1;
+        for ( int y = boxLength; y < height; y += boxLength) {
+            if (lineNumber % 5 == 0) {
+                strokeWeight(3);
+            } else {
+                strokeWeight(1);
+            }
+            line(0, y, width, y);
+            lineNumber++;
+        }
+
+        // Another solution
+        /*
+        for (int counter = 1; ; counter++) {
+            if (counter % 5 == 1) {
+                strokeWeight(3);
+            } else {
+                strokeWeight(1);
+            }
+            line(counter * boxLength, 0, counter * boxLength, height);
+        }
+         */
     }
 
+    /**
+     * Draw dots to cover the entire screen with gaps between each
+     * @param gap the number of pixels between each dot
+     * @param radius the radius of one dot
+     */
     public void drawDotPaper(int gap, int radius) {
-        // TODO: Finish this method.
+        stroke(230, 30, 100);
+
+        // Outer loop chooses a value
+            // Inner loop then does all of its values, when finished outer loop changes once
+
+        for (int x = 0; x < width; x += gap) {
+            for (int y = 0; y < height; y += gap) {
+                ellipse(x, y, 2 * radius, 2 * radius);
+            }
+        }
     }
+
 
     public void drawPrettyPaper() {
-        // TODO: Finish this method.
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                // Each and every (x, y) combination
+                int chosenColor = color((float) x / width * 360, (float) y / height * 100, 100); // x is hue, y is saturation, 100 is brightness
+                set(x, y, chosenColor);
+            }
+        }
     }
 
 
